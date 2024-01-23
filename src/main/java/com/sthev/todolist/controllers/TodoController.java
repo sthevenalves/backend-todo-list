085b7181accf7be9967fd73ef85120415d306463
Stheven <sthevenalvess@gmail.com>
2024-01-23 15:28:07 -0300

    feat: endpoints

---
+++ src/main/java/com/sthev/todolist/controllers/TodoController.java
@@ -0,0 +1,39 @@
+package com.sthev.todolist.controllers;
+
+import com.sthev.todolist.domain.Todo;
+import com.sthev.todolist.servicies.TodoService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+
+    @Autowired
+    TodoService service;
+
+    @GetMapping
+    public ResponseEntity<List<Todo>> findAll(){
+        return ResponseEntity.ok(service.findAll());
+    }
+    @PostMapping
+    public ResponseEntity<List<Todo>> save(@RequestBody @Valid Todo todo){
+        service.save(todo);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+    @PutMapping
+    public ResponseEntity<Void> replace(@RequestBody @Valid Todo todo){
+        service.replace(todo);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
